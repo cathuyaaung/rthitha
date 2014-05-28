fe14c65f1f4544b5ef226d3638e26cdce482374e
Thomas Thuya Aung <thu.ya.a@gmail.com>
2014-05-28 16:43:25 +0000

    34

--- settings.py
+++ settings.py
@@ -106,10 +106,15 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'rthitha.urls'
 
+import os
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    PROJECT_PATH + '/templates/',
 )
 
 INSTALLED_APPS = (
--- views.py
+++ views.py
@@ -18,4 +18,4 @@ def home(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('rango/index.html', context_dict, context)	
+    return render_to_response('index.html', context_dict, context)
