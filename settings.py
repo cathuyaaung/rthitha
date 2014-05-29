2a27d4efd022f2dc21495b8991e5718271e362dd
cizrage <thu.ya.a@gmail.com>
2014-05-29 21:56:59 +0800

    asdf

--- settings.py
+++ settings.py
@@ -64,7 +64,8 @@ MEDIA_URL = '/media/'
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
 # STATIC_ROOT = '/home/cizrage/rthitha/static'
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -80,6 +81,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    # "E:/Git/rthitha/static",
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
