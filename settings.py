22b62b3c2c727da1e78618b27ba869f7d2664691
cizrage <thu.ya.a@gmail.com>
2014-05-29 22:39:38 +0800

    asdf

--- settings.py
+++ settings.py
@@ -14,12 +14,24 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        # 'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        # 'NAME': '',                      # Or path to database file if using sqlite3.
+        # 'USER': '',                      # Not used with sqlite3.
+        # 'PASSWORD': '',                  # Not used with sqlite3.
+        # 'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'rthitha',
+        # 'USER': 'django_user',
+        # 'PASSWORD': 'thepassword'
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cizrage$rthitha',
+        'USER': 'cizrage',
+        'PASSWORD': 'ygmfromthuya'
+
+
     }
 }
 
