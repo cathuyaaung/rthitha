f3b0709296375550dff934436b53bc58aa2774c6
cizrage <thu.ya.a@gmail.com>
2014-05-29 23:45:34 +0800

    test

--- settings.py
+++ settings.py
@@ -141,12 +141,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'movies',
+    'django.contrib.staticfiles',    
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    #'movies',
 )
 
 # A sample logging configuration. The only tangible logging
