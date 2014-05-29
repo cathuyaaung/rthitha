c12310aa1b271819133f6ace4be770bc4f9703b1
cizrage <thu.ya.a@gmail.com>
2014-05-29 23:25:24 +0800

    asdf

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 from rthitha import movies
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
