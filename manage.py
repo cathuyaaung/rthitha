067568074a9e3c872efc64d9c042c34d8a350c8a
Thomas Thuya Aung <thu.ya.a@gmail.com>
2014-06-09 04:44:19 +0000

    migration

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os, sys
 
 if __name__ == "__main__":
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rthitha.settings")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 	from django.core.management import execute_from_command_line
 
