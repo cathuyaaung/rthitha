0dc960139ea9606114d28155c2a15971c2cae82a
Thomas Thuya Aung <thu.ya.a@gmail.com>
2014-06-09 03:57:20 +0000

    social auth

--- rthithasite/urls.py
+++ rthithasite/urls.py
@@ -10,11 +10,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'', include('social_auth.urls')),
-	#url(r'^logout/$', 'rthithasite.views.logout'),
+
+    url(r'', include('social_auth.urls')),
+	url(r'^logout/$', 'rthithasite.views.logout'),
 	
 	url(r'^test/(?P<movie_title_url>\w+)/$', views.test, name='test'),
-
 
 	#url(r'^login-error/$', 'rthithasite.views.loginerror'),
 
