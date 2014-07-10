525cc9cfbfdd715dc89520b005e0ea7bc401ecf2
Thomas Thuya Aung <thu.ya.a@gmail.com>
2014-07-10 12:38:01 +0000

    commit

--- rthithasite/static/js/rthitha.js
+++ rthithasite/static/js/rthitha.js
@@ -5,38 +5,80 @@ $(document).ready(function(){
 		$('.comment-form form').bindPostCommentHandler();
 		$('.reply-show-hide-form').bindCommentFormShowHideHandler();
 		$('.comment-form-btn-cancel').bindCommentFormCancelHandler();
+		$('.movielink').bindShowPlayer();
 	});
-	
+
 	(function( $ ){
-	$.fn.bindCommentFormCancelHandler = function() {	
+	$.fn.bindShowPlayer = function() {
+
+		$('div.location').each(function() {
+			var $locationdiv = $(this)
+
+			$("a.movielink", $locationdiv).click(function(e) {
+                var id = $(this).attr('id');
+                var target = $(this).attr('target');
+
+      var d = $(this).data('srt');
+      alert(d);
+      var subtitles1 = d;
+
+				if($(document).find('.targetlocation').length){
+					$targetlocation = $('.targetlocation');
+
+					if(id==1){
+					    strHtml = "<iframe id='f' name='widget' src='"+target+
+                        "' width='852' height='510' style='float:center'></iframe>";
+                        $targetlocation.html(strHtml);
+					}else{
+
+					    strHtml = "<video width='852' height='510' controls><source src='"+target+
+                        "'><track kind='captions' srclang='en' label='English'"+
+                        "src='"+subtitles1+"' default></track></video>";
+
+                        $targetlocation.html(strHtml);
+					}
+				}
+			});
+		});
+
+		$(document).on('click', '.reply-show-hide-form', function(){
+			$('.comment-form').hide();
+			$('#'+$(this).attr('target')).show();
+		});
+		};
+	})( jQuery );
+
+
+	(function( $ ){
+	$.fn.bindCommentFormCancelHandler = function() {
 
 	//control to show hide comment form when clicked on cancel
 		$('div.comment-form').each(function() {
 			var $commentform = $(this)
-			
+
 			$("button.comment-form-btn-cancel", $commentform).click(function(e) {
 				e.preventDefault();
 				$commentform.toggle();
 				$("div.comment-form").not($commentform).hide();
 				return false;
-			});		
-		});	
-	
-	
+			});
+		});
+
+
 		};
 	})( jQuery );
-	
+
 	//control to show hide comment forms when clicked on 'reply' 'post comment'
 	//$('.reply-show-hide-form').click(function(){
 	(function( $ ){
-	$.fn.bindCommentFormShowHideHandler = function() {	
-		$(document).on('click', '.reply-show-hide-form', function(){ 
+	$.fn.bindCommentFormShowHideHandler = function() {
+		$(document).on('click', '.reply-show-hide-form', function(){
 			$('.comment-form').hide();
 			$('#'+$(this).attr('target')).show();
-		});	
+		});
 		};
 	})( jQuery );
-	
+
 
 
 	(function( $ ){
@@ -47,9 +89,9 @@ $(document).ready(function(){
 			//$(this).find('input.submit-preview').remove();
 			$(this).submit(function() {
 				commentform = this;
-				commentformdiv = $(this).parent();				
+				commentformdiv = $(this).parent();
 				var comment_text = $('#id_comment').val();
-				
+
 				$.ajax({
 					type: "POST",
 					data: $(commentform).serialize(),
@@ -58,29 +100,29 @@ $(document).ready(function(){
 					dataType: "html",
 					success: function(html, textStatus) {
 						//alert(html);
-						
+
 
 						$(commentformdiv).hide();
-						
+
 						//if($('#parent_id').length){
 						if($(commentformdiv).find('#parent_id').length){
 
 							//replying comment
-							
+
 							$commentbodydiv = commentformdiv.parent();
 							if($commentbodydiv.find('.comment-children').length){
 								$childrenul = $commentbodydiv.find('.comment-children');
 							}else{
-								var $childrenul = $("<ul>", {id: "foo", class: "comment-children"});							
+								var $childrenul = $("<ul>", {id: "foo", class: "comment-children"});
 								$childrenul.appendTo($commentbodydiv);
 							}
-							var li = $('<li>').prependTo($childrenul);							
+							var li = $('<li>').prependTo($childrenul);
 							$(html).appendTo(li).fadeIn(500);
-							
-							
+
+
 						}else{
-						
-						
+
+
 							//root
 							if($(document).find('.comment-root').length){
 								$commentroot = $('.comment-root');
@@ -89,21 +131,21 @@ $(document).ready(function(){
 							else{
 								var paneldiv = $('<div>', {class:"panel"});
 								paneldiv.insertAfter(commentformdiv);
-								
+
 								var rootul =  $('<ul>', {class:"comment-root"}).appendTo(paneldiv);
 								var div =  $('<div>').appendTo(rootul);
 								var li = $('<li>').appendTo(div);
 								$(html).appendTo(li);
 							}
-							
-							
+
+
 
 						}
-						
-						
-						
+
+
+
 						$(html).find('.reply-show-hide-form').bindCommentFormShowHideHandler();
-						$(html).find('.comment-form-btn-cancel').bindCommentFormCancelHandler();		
+						$(html).find('.comment-form-btn-cancel').bindCommentFormCancelHandler();
 						$(html).find('.comment-form form').bindPostCommentHandler();
 					},
 					error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -115,9 +157,9 @@ $(document).ready(function(){
 		}); //each
 	};
 	})( jQuery );
-	
+
 	if (window.location.hash == '#_=_') {
       window.location.hash = '';
    }
-	
+
 });
